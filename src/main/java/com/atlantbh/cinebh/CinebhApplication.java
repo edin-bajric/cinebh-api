389a93b813c5ecaa0e86f9abbb8f23a4c914d023
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-14 15:31:34 +0200

    excluded DataSourceAutoConfiguration from main app because a db dependency expects a connection string

--- src/main/java/com/atlantbh/cinebh/CinebhApplication.java
+++ src/main/java/com/atlantbh/cinebh/CinebhApplication.java
@@ -2,8 +2,9 @@ package com.atlantbh.cinebh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CinebhApplication {
 
 	public static void main(String[] args) {
