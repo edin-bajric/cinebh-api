7fc20528d0d6ecec210ad680a2a7e0a8efd00e25
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-14 14:51:47 +0200

    first commit

---
+++ src/test/java/com/atlantbh/cinebh/CinebhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlantbh.cinebh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinebhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
