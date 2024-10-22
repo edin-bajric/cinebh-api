df34c66a6cfc44933ba8683477d4577991a18ffc
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-22 12:01:09 +0200

    removed unused import statements

--- src/main/java/com/atlantbh/cinebh/core/services/MovieService.java
+++ src/main/java/com/atlantbh/cinebh/core/services/MovieService.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
 import java.time.LocalDate;
-import java.util.Calendar;
 
 @Service
 @RequiredArgsConstructor
