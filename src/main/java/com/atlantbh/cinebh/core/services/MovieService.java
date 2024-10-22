de024aa520fee5398960fd226090750e183e1234
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-22 11:10:47 +0200

    implemented fully paginated retrieval of movies, changed some movie table column types

--- src/main/java/com/atlantbh/cinebh/core/services/MovieService.java
+++ src/main/java/com/atlantbh/cinebh/core/services/MovieService.java
@@ -3,16 +3,16 @@ package com.atlantbh.cinebh.core.services;
 import com.atlantbh.cinebh.core.models.Movie;
 import com.atlantbh.cinebh.core.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
 
-    public List<Movie> getMovies() {
-        return movieRepository.findAll();
+    public Page<Movie> getMovies(Pageable pageable) {
+        return movieRepository.findAll(pageable);
     }
 }
