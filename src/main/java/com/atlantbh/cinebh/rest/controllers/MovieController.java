c1da9c072385696224927e15954db8bed69a0347
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-22 11:10:47 +0200

    implemented fully paginated retrieval of movies, changed some movie table column types

--- src/main/java/com/atlantbh/cinebh/rest/controllers/MovieController.java
+++ src/main/java/com/atlantbh/cinebh/rest/controllers/MovieController.java
@@ -3,12 +3,14 @@ package com.atlantbh.cinebh.rest.controllers;
 import com.atlantbh.cinebh.core.models.Movie;
 import com.atlantbh.cinebh.core.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/movies")
@@ -17,7 +19,9 @@ public class MovieController {
     private final MovieService movieService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
-    public ResponseEntity<List<Movie>> getMovies() {
-        return ResponseEntity.ok(movieService.getMovies());
+    public ResponseEntity<Page<Movie>> getMovies(@RequestParam(name = "page", defaultValue = "0") int page,
+                                                 @RequestParam(name = "size", defaultValue = "4") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(movieService.getMovies(pageable));
     }
 }
---
+++ src/main/resources/db/migration/V3__change_movie_data_type.sql
@@ -0,0 +1,2 @@
+ALTER TABLE movie ALTER COLUMN title TYPE varchar (255);
+ALTER TABLE movie ALTER COLUMN trailerurl TYPE varchar (255);
