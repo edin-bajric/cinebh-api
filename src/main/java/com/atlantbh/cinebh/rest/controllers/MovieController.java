2895eb08496c881bde1c4c1f240debb9f692f2da
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-29 12:46:51 +0100

    now using @GetMapping for get requests

--- src/main/java/com/atlantbh/cinebh/rest/controllers/MovieController.java
+++ src/main/java/com/atlantbh/cinebh/rest/controllers/MovieController.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/movies")
@@ -18,21 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
     private final MovieService movieService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @GetMapping("/")
     public ResponseEntity<Page<Movie>> getMovies(@RequestParam(name = "page", defaultValue = "0") int page,
                                                  @RequestParam(name = "size", defaultValue = "4") int size) {
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(movieService.getMovies(pageable));
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/currently-showing")
+    @GetMapping("/currently-showing")
     public ResponseEntity<Page<Movie>> currentlyShowing(@RequestParam(name = "page", defaultValue = "0") int page,
                                                         @RequestParam(name = "size", defaultValue = "4") int size) {
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(movieService.getCurrentlyShowing(pageable));
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/upcoming")
+    @GetMapping("/upcoming")
     public ResponseEntity<Page<Movie>> upcomingMovies(@RequestParam(name = "page", defaultValue = "0") int page,
                                                       @RequestParam(name = "size", defaultValue = "4") int size) {
         Pageable pageable = PageRequest.of(page, size);
--- src/main/java/com/atlantbh/cinebh/rest/controllers/VenueController.java
+++ src/main/java/com/atlantbh/cinebh/rest/controllers/VenueController.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/venues")
@@ -18,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class VenueController {
     private final VenueService venueService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @GetMapping("/")
     public ResponseEntity<Page<Venue>> getVenues(@RequestParam (name = "page", defaultValue = "0") int page,
                                                  @RequestParam(name = "size", defaultValue = "4") int size) {
         Pageable pageable = PageRequest.of(page, size);
