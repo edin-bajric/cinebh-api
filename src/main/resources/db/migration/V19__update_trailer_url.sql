3b0e9d34fb1844d2e4c704b88a979763444649b2
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-22 11:23:13 +0100

    updated the movie trailers to use embed links

---
+++ src/main/resources/db/migration/V19__update_trailer_url.sql
@@ -0,0 +1,10 @@
+update movie set trailerurl = 'https://www.youtube.com/embed/d9MyW72ELq0?si=rygI77MGc-e9wOQJ' where title = 'Avatar: The Way of Water';
+update movie set trailerurl = 'https://www.youtube.com/embed/As-vKW4ZboU?si=jBo0vSe120mDgfCh' where title = 'Beetlejuice Beetlejuice';
+update movie set trailerurl = 'https://www.youtube.com/embed/_OKAwz2MsJs?si=ixu0j95glhucE2jE' where title = 'Joker: Folie à Deux';
+update movie set trailerurl = 'https://www.youtube.com/embed/OAZWXUkrjPc?si=RZe3NjEOfTuO_q6p' where title = 'Napoleon';
+update movie set trailerurl = 'https://www.youtube.com/embed/__2bjWbetsA?si=JFRLDU_ANiuQK5Un' where title = 'Venom: The Last Dance';
+update movie set trailerurl = 'https://www.youtube.com/embed/zaPcin5knJk?si=p4KkCV3Q8pXxts7W' where title = 'Terrifier 3';
+update movie set trailerurl = 'https://www.youtube.com/embed/4rgYUipGJNo?si=vQkZHQXT3VXK00ps' where title = 'Gladiator II';
+update movie set trailerurl = 'https://www.youtube.com/embed/4fw2PIpndnM?si=SeVSE6mQqfFhE0Fd' where title = 'Hellboy: The Crooked Man';
+update movie set trailerurl = 'https://www.youtube.com/embed/OzY2r2JXsDM?si=yRcTjI8sizRPj_gS' where title = 'Alien: Romulus';
+update movie set trailerurl = 'https://www.youtube.com/embed/67vbA5ZJdKQ?si=dKKUIGsmaOyUyjCj' where title = 'The Wild Robot';
