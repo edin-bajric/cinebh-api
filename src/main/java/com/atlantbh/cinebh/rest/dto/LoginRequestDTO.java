0b16e1645b22b80e3de6c7652bc291a1a448d5d5
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-26 00:43:43 +0100

    implemented logout endpoint where when the user logs out his jwt is put in a blacklist, also added required fields for email and password in the LoginRequestDTO

--- src/main/java/com/atlantbh/cinebh/rest/dto/LoginRequestDTO.java
+++ src/main/java/com/atlantbh/cinebh/rest/dto/LoginRequestDTO.java
@@ -1,5 +1,8 @@
 package com.atlantbh.cinebh.rest.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +11,8 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public class LoginRequestDTO {
+    @NotBlank(message = "Email is required.")
     private String email;
+    @NotBlank(message = "Password is required.")
     private String password;
 }
