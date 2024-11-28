23cec3dd350fa06cb0d417c4653dbb716b5ca9b1
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-28 12:35:35 +0100

    added code validation and password reset functionalities

--- src/main/java/com/atlantbh/cinebh/core/repositories/PasswordResetCodeRepository.java
+++ src/main/java/com/atlantbh/cinebh/core/repositories/PasswordResetCodeRepository.java
@@ -1,11 +1,19 @@
 package com.atlantbh.cinebh.core.repositories;
 
+import com.atlantbh.cinebh.core.models.AppUser;
 import com.atlantbh.cinebh.core.models.PasswordResetCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PasswordResetCodeRepository extends JpaRepository<PasswordResetCode, UUID> {
+    Optional<PasswordResetCode> findByAppUserAndCodeAndUsed(AppUser appUser, String code, boolean used);
+    @Modifying
+    @Query("UPDATE PasswordResetCode prc SET prc.used = :used WHERE prc.appUser = :appUser AND prc.code = :code")
+    void updateUsedByAppUserAndCode(AppUser appUser, String code, boolean used);
 }
