fa5176bd00a1cab9c1b94872876d621ad268def6
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-01 00:45:02 +0100

    added RequestParam for UserController

--- src/main/java/com/atlantbh/cinebh/rest/controllers/AppUserController.java
+++ src/main/java/com/atlantbh/cinebh/rest/controllers/AppUserController.java
@@ -4,10 +4,7 @@ import com.atlantbh.cinebh.core.services.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/users")
@@ -16,19 +13,19 @@ public class AppUserController {
     private final AppUserService appUserService;
 
     @PostMapping("/send-email")
-    public ResponseEntity<Void> sendEmail(String email) {
+    public ResponseEntity<Void> sendEmail(@RequestParam String email) {
         appUserService.sendEmail(email);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PatchMapping("/validate-code")
-    public ResponseEntity<Void> validateCode(String email, String code) {
+    public ResponseEntity<Void> validateCode(@RequestParam String email, @RequestParam String code) {
         appUserService.validateCode(email, code);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PatchMapping("/reset-password")
-    public ResponseEntity<Void> resetPassword(String email, String newPassword) {
+    public ResponseEntity<Void> resetPassword(@RequestParam String email, @RequestParam String newPassword) {
         appUserService.resetPassword(email, newPassword);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
