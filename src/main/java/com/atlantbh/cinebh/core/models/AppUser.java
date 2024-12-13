ad8af9032c0d054a241af09ed21a7a8fabf5fcd8
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-13 17:32:36 +0100

    removed wildcard imports from AppUser

--- src/main/java/com/atlantbh/cinebh/core/models/AppUser.java
+++ src/main/java/com/atlantbh/cinebh/core/models/AppUser.java
@@ -1,7 +1,14 @@
 package com.atlantbh.cinebh.core.models;
 
 import com.atlantbh.cinebh.core.models.enums.UserType;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
