f053f82f5e53e85dbc37cd4db143bd8279308db1
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-13 18:23:38 +0100

    Added validation to email parameter in AppUserRepository methods

--- src/main/java/com/atlantbh/cinebh/core/repositories/AppUserRepository.java
+++ src/main/java/com/atlantbh/cinebh/core/repositories/AppUserRepository.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
     Optional<AppUser> findByEmail(String email);
-    boolean existsByEmail(String email);
+    boolean existsByEmail(@Email @NotBlank String email);
     @Modifying
     @Query("UPDATE AppUser u SET u.password = :password WHERE u.email = :email")
     void updatePasswordByEmail(@Email @NotBlank String email, String password);
