a763e9c83365c4e680c0b33d1207e38028ab7f8b
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-13 17:30:46 +0100

    removed wildcard imports

--- src/main/java/com/atlantbh/cinebh/core/models/PasswordResetCode.java
+++ src/main/java/com/atlantbh/cinebh/core/models/PasswordResetCode.java
@@ -1,6 +1,12 @@
 package com.atlantbh.cinebh.core.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
