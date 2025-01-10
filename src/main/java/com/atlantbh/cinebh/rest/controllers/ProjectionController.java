f7674814ffee76c3a832258b341151c9ffc2b823
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-01-11 00:20:03 +0100

    added functionality for returning projection details for the first step of seat selection

---
+++ src/main/java/com/atlantbh/cinebh/rest/controllers/ProjectionController.java
@@ -0,0 +1,24 @@
+package com.atlantbh.cinebh.rest.controllers;
+
+import com.atlantbh.cinebh.core.services.ProjectionService;
+import com.atlantbh.cinebh.rest.dto.ProjectionDetailsDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/projections")
+@RequiredArgsConstructor
+public class ProjectionController {
+    private final ProjectionService projectionService;
+
+    @GetMapping("/projection-details/{movieId}")
+    public ResponseEntity<ProjectionDetailsDTO> getProjectionDetails(@PathVariable("movieId") UUID movieId) {
+        return ResponseEntity.ok(projectionService.getProjectionDetails(movieId));
+    }
+}
